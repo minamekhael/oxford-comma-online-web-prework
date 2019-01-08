69641a631cd109f34a39d5fc96ec116d2beb5a09
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-08 02:04:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,12 +3,5 @@ def oxford_comma(array)
    array[-2] << "and"
   puts 
   array.join
- elsif array == 1 
- puts 
- array.join 
-  elsif array.length > 2
-    array[-1].join("and ")
-  puts 
-  array.join(", ")
-  end
+end
 end
