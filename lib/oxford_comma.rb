60e99529a6057ad7286fe6686335d33e171080b8
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-08 02:20:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1 
     array.join
   elsif array.length == 2
-    array.join("and")
+    array.join(" and ")
   else 
     array.push[-1] = "and #{array[-1]}"
     array.join(", ")
